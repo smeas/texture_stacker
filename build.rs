7fd4ff4ba5cc79ddfe6580cdb1949294b0e7bcc9
Jonatan Johansson <smeas@users.noreply.github.com>
2022-08-08 22:22:15 +0200

    Set version in file metadata

---
+++ build.rs
@@ -0,0 +1,11 @@
+use winres::VersionInfo;
+
+fn main() {
+    let version = (0, 2, 0, 0);
+    let version_num = version.0 << 48 | version.1 << 32 | version.2 << 16 | version.3;
+
+    let mut res = winres::WindowsResource::new();
+    res.set_version_info(VersionInfo::FILEVERSION, version_num);
+    res.set_version_info(VersionInfo::PRODUCTVERSION, version_num);
+    res.compile().unwrap();
+}
